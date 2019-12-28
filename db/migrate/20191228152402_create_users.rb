ea9bbfd35c6f11e782752df8d43aaf5a30e2306e
inkanimal <theatomproject@gmail.com>
2019-12-28 10:24:25 -0500

    update user scaffold

--- db/migrate/20191228152402_create_users.rb
+++ db/migrate/20191228152402_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.text :username
-      t.text :email
-      t.password_digest :password
+      t.string :username
+      t.string :email
+      t.string :password_digest
 
       t.timestamps
     end
