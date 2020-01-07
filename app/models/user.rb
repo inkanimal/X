bda8e1f24821d089be8a94046161fbeec2898b6d
inkanimal <theatomproject@gmail.com>
2020-01-07 10:27:44 -0500

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, :email, presence: true
+    validates :username, uniqueness: true
+    validates :email, uniqueness: true
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   # attributes :id, :username, :email, :password_digest
-  include FastJsonapi::ObjectSerializer
+  # include FastJsonapi::ObjectSerializer
   attributes :username, :email, :password
 end
