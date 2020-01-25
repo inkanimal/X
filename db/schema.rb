7f24d6328b3705b208a82f962cd29cdae91a8b2c
inkanimal <theatomproject@gmail.com>
2020-01-25 12:33:35 -0500

    add manufacturer to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_19_043732) do
+ActiveRecord::Schema.define(version: 2020_01_25_173235) do
 
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_01_19_043732) do
     t.string "product_name"
     t.text "description"
     t.integer "price"
+    t.string "manufacturer"
   end
 
   create_table "users", force: :cascade do |t|
