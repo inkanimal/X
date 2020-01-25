5890c3f89a2eb83303cc3120b7dde85bb0890a97
inkanimal <theatomproject@gmail.com>
2020-01-25 12:40:32 -0500

    add category to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_173705) do
+ActiveRecord::Schema.define(version: 2020_01_25_173907) do
 
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_01_25_173705) do
     t.string "manufacturer"
     t.string "size"
     t.string "color"
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
